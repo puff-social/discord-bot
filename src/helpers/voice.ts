c3ea52c6dc8f45954ace5f8824f102de37966c59
Dustin Rouillard <code@dstn.to>
2023-10-29 17:44:35 -0600

    Remove debugging logs

--- src/helpers/voice.ts
+++ src/helpers/voice.ts
@@ -4,9 +4,7 @@ import { VoiceTextChannels, VoiceToText } from '../constants';
 
 export async function changeVoiceText(id: string, user: string) {
   const matchingTextChannel = VoiceToText[id];
-  console.log(matchingTextChannel, 1);
   const otherChannels = Object.values(VoiceTextChannels).filter((id) => id != matchingTextChannel);
-  console.log(otherChannels, 2);
 
   for await (const channel of otherChannels) {
     const chan = await client.channels.fetch(channel);
