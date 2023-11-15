37055a405348b56aa645ee4f577dcd7b797600cd
Dustin Rouillard <code@dstn.to>
2023-11-14 19:59:15 -0700

    Wait soundboard for 500ms

--- src/helpers/voice.ts
+++ src/helpers/voice.ts
@@ -22,7 +22,9 @@ export async function changeVoiceText(id: string, user: string) {
 
 export async function switchSoundboardPermissions(oldChannel: Channel, newChannel: Channel, user: string) {
   if (oldChannel && oldChannel instanceof VoiceChannel) await oldChannel.permissionOverwrites.delete(user);
-  if (newChannel && newChannel instanceof VoiceChannel) await newChannel.permissionOverwrites.create(user, {
-    UseSoundboard: true
-  });
+  setTimeout(() => {
+    if (newChannel && newChannel instanceof VoiceChannel) await newChannel.permissionOverwrites.create(user, {
+      UseSoundboard: true
+    });
+  }, 500);
 }
