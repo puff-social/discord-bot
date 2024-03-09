5d09bbcb1829e58fc9bfbf6bba05b873648d1bd3
Dustin Rouillard <code@dstn.to>
2024-03-08 23:24:06 -0700

    Add check for private channels

--- src/api.ts
+++ src/api.ts
@@ -12,8 +12,10 @@ server.get("/health", (req, res) => res.status(204).send());
 server.get('/channels/:id', async (req: FastifyRequest<{ Params: { id: string } }>, res) => {
   const cached = client.channels.cache.get(req.params.id);
   if (cached && cached instanceof VoiceChannel) {
+    if (!cached.permissionsFor(cached.guild.id).has(PermissionFlagsBits.ViewChannel)) return res.status(400).send({ code: 'private_channel' });
+
     let link: string;
-    if (cached.permissionsFor(cached.guild.id).has(PermissionFlagsBits.Connect & PermissionFlagsBits.ViewChannel)) {
+    if (cached.permissionsFor(cached.guild.id).has(PermissionFlagsBits.Connect)) {
       const invites = await cached.fetchInvites();
       if (!invites.first()) link = `https://discord.gg/${(await cached.createInvite({ maxAge: 0 })).code}`;
       link = `https://discord.gg/${invites.first().code}`;
@@ -28,8 +30,10 @@ server.get('/channels/:id', async (req: FastifyRequest<{ Params: { id: string } 
 
   const channel = await client.channels.fetch(req.params.id);
   if (!channel || !(channel instanceof VoiceChannel)) return res.status(404).send();
+  if (!channel.permissionsFor(channel.guild.id).has(PermissionFlagsBits.ViewChannel)) return res.status(400).send({ code: 'private_channel' });
+
   let link: string;
-  if (channel.permissionsFor(channel.guild.id).has(PermissionFlagsBits.Connect & PermissionFlagsBits.ViewChannel)) {
+  if (channel.permissionsFor(channel.guild.id).has(PermissionFlagsBits.Connect)) {
     const invites = await channel.fetchInvites();
     if (!invites.first()) link = `https://discord.gg/${(await channel.createInvite({ maxAge: 0 })).code}`;
     link = `https://discord.gg/${invites.first().code}`;
